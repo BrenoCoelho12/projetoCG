7e0d5ba8e31bf66d77fe3cfbf173add46f46ae4b
Lufec <30414428+Lufec@users.noreply.github.com>
2020-07-25 15:47:58 -0300

    Mais elipses e 6 luzes

--- main.cpp
+++ main.cpp
@@ -6,19 +6,32 @@
 #include <math.h>
 #include <iostream>
 
-static int year1 = 3, year2 = 1, year3 = 2 , day1 = 1 ,day2 = 2 ,day3 = 3, camera = 1;
-float  pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, zoom  = 10, xc=0 ,yc=0 ,zc=0;
+static int day1 = 1 ,day2 = 2 ,day3 = 3, camera = 1;
+float  rot1 = 0, rot2 = 0, rot3 = 0, zoom  = 10;
 float mouseX = 0, mouseY = 0, mouseXp = 0, mouseYp = 0, movX=0, movY=0, cx = 0, cy = 0;
 bool primeiroMov = 1;
 GLsizei alt,larg;
 
-   float d2r = 3.14159265 / 180.0;
-   static float theta = 0.0, thetar = 0.0, phi = 0.0, phir = 0.0;
+float d2r = 3.14159265 / 180.0;
+static float theta = 0.0, thetar = 0.0, phi = 0.0, phir = 0.0;
 
 
-float lightPos[] = { 0.0, 0.0, 0.0, 1.0 }; // Spotlight position.
-static float spotAngle = 60; // Spotlight cone half-angle.
-static float spotExponent = 1.0; // Spotlight exponent = attenuation factor.
+float lightPos0[] = { 1.1, 0.0, 0.0, 0.0}; // Spotlight position.
+float lightPos1[] = { 0.0, 1.1, 0.0, 0.0}; // Spotlight position.
+float lightPos2[] = { 0.0, 0.0, 1.1, 0.0}; // Spotlight position.
+float lightPos3[] = { -1.1, 0.0, 0.0, 0.0}; // Spotlight position.
+float lightPos4[] = { 0.0, -1.1, 0.0, 0.0}; // Spotlight position.
+float lightPos5[] = { 0.0, 0.0, -1.1, 0.0}; // Spotlight position.
+
+float lightDir0[] = { 1.0, 0.0, 0.0};
+float lightDir1[] = { 0.0, 1.0, 0.0};
+float lightDir2[] = { 0.0, 0.0, 1.0};
+float lightDir3[] = { -1.0, 0.0, 0.0};
+float lightDir4[] = { 0.0, -1.0, 0.0};
+float lightDir5[] = { 0.0, 0.0, -1.0};
+
+static float spotAngle = 180; // Spotlight cone half-angle.
+static float spotExponent = 2.0; // Spotlight exponent = attenuation factor.
 GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
 GLfloat mat_shininess[] = { 50.0 };
 
@@ -54,42 +67,92 @@ void init(void)
 	venText = loadTexture(ven);
 	delete ven;
 
-
+////////////////////
     glEnable(GL_LIGHTING);
 	float lightAmb[] = { 0.0, 0.0, 0.0, 1.0 };
-	float lightDifAndSpec[] = { 1.0, 1.0, 1.0, 1.0 };
+	float lightDifAndSpec[] = { 0.5, 0.5, 0.5, 1.0 };
 
 	float globAmb[] = { 0.5, 0.5, 0.5, 1.0 };
 	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDifAndSpec);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, lightDifAndSpec);
+	glLightf(GL_LIGHT0,GL_SPOT_CUTOFF, spotAngle);
+	glLightf(GL_LIGHT0,GL_SPOT_EXPONENT, spotExponent);
+
+	glLightfv(GL_LIGHT1, GL_AMBIENT, lightAmb);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightDifAndSpec);
+	glLightfv(GL_LIGHT1, GL_SPECULAR, lightDifAndSpec);
+	glLightf(GL_LIGHT1,GL_SPOT_CUTOFF, spotAngle);
+	glLightf(GL_LIGHT1,GL_SPOT_EXPONENT, spotExponent);
+
+	glLightfv(GL_LIGHT2, GL_AMBIENT, lightAmb);
+	glLightfv(GL_LIGHT2, GL_DIFFUSE, lightDifAndSpec);
+	glLightfv(GL_LIGHT2, GL_SPECULAR, lightDifAndSpec);
+	glLightf(GL_LIGHT2,GL_SPOT_CUTOFF, spotAngle);
+	glLightf(GL_LIGHT2,GL_SPOT_EXPONENT, spotExponent);
+
+	glLightfv(GL_LIGHT3, GL_AMBIENT, lightAmb);
+	glLightfv(GL_LIGHT3, GL_DIFFUSE, lightDifAndSpec);
+	glLightfv(GL_LIGHT3, GL_SPECULAR, lightDifAndSpec);
+	glLightf(GL_LIGHT3,GL_SPOT_CUTOFF, spotAngle);
+	glLightf(GL_LIGHT3,GL_SPOT_EXPONENT, spotExponent);
+
+	glLightfv(GL_LIGHT4, GL_AMBIENT, lightAmb);
+	glLightfv(GL_LIGHT4, GL_DIFFUSE, lightDifAndSpec);
+	glLightfv(GL_LIGHT4, GL_SPECULAR, lightDifAndSpec);
+	glLightf(GL_LIGHT4,GL_SPOT_CUTOFF, spotAngle);
+	glLightf(GL_LIGHT4,GL_SPOT_EXPONENT, spotExponent);
+
+	glLightfv(GL_LIGHT5, GL_AMBIENT, lightAmb);
+	glLightfv(GL_LIGHT5, GL_DIFFUSE, lightDifAndSpec);
+	glLightfv(GL_LIGHT5, GL_SPECULAR, lightDifAndSpec);
+	glLightf(GL_LIGHT5,GL_SPOT_CUTOFF, spotAngle);
+	glLightf(GL_LIGHT5,GL_SPOT_EXPONENT, spotExponent);
+
+
+   glLightfv(GL_LIGHT0,GL_POSITION,lightPos0);
+   glLightfv(GL_LIGHT1,GL_POSITION,lightPos1);
+   glLightfv(GL_LIGHT2,GL_POSITION,lightPos2);
+   glLightfv(GL_LIGHT3,GL_POSITION,lightPos3);
+   glLightfv(GL_LIGHT4,GL_POSITION,lightPos4);
+   glLightfv(GL_LIGHT5,GL_POSITION,lightPos5);
+
+   glLightfv(GL_LIGHT0,GL_SPOT_DIRECTION,lightDir0);
+   glLightfv(GL_LIGHT1,GL_SPOT_DIRECTION,lightDir1);
+   glLightfv(GL_LIGHT2,GL_SPOT_DIRECTION,lightDir2);
+   glLightfv(GL_LIGHT3,GL_SPOT_DIRECTION,lightDir3);
+   glLightfv(GL_LIGHT4,GL_SPOT_DIRECTION,lightDir4);
+   glLightfv(GL_LIGHT5,GL_SPOT_DIRECTION,lightDir5);
 
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globAmb);
 	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
 	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
-	glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, spotAngle);
-	glLightf(GL_LIGHT0, GL_SPOT_EXPONENT, spotExponent);
 
+/////////////////////
 
    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
+   glEnable(GL_LIGHT1);
+   glEnable(GL_LIGHT2);
+   glEnable(GL_LIGHT3);
+   glEnable(GL_LIGHT4);
+   glEnable(GL_LIGHT5);
    glEnable(GL_DEPTH_TEST);
 }
 
 void animate(int n){
 	//thetar = theta*d2r;
-	pos1 += year1;
-	pos2 += year2;
-	pos3 += day1;
-	pos4 += day2;
-	if (pos1, pos2, pos3, pos4 > 360.0){
-		pos1, pos2, pos3, pos4 -= 360.0;
+	rot1 += day1;
+	rot2 += day2;
+	rot3 += day3;
+	if (rot1,rot2,rot3 > 360.0){
+        rot1,rot2,rot3 -= 360.0;
 	}
-    theta +=1;
+    theta +=3;
+    phi+=1;
 	glutPostRedisplay();
 
 
@@ -99,18 +162,17 @@ void display(void)
 {
 
    thetar = theta * d2r;
-
+   phir = phi*d2r;
 
    glLoadIdentity();
-
    if(camera == 1){
-        gluLookAt (cx, 1.5*cy, zoom, cx+xc, cy , 1.0, 0.0, 1.0, 0.0);
+        gluLookAt (cx, cy, zoom, cx, cy , 0.0, 0.0, 1.0, 0.0);
    }
    if(camera == 2){
-        gluLookAt (cx +xc, zoom , cy +xc, cx, 1.0,cy , 1.0, 0.5, -1.0);
+        gluLookAt (cx, zoom , cy, cx, 0.0,cy , 1.0, 0.5, 1.0);
    }
    if(camera == 3){
-        gluLookAt (cx+xc, zoom, cy, 1.0, cx + xc , cy, 0.0, 0.0 , 1.0);
+        gluLookAt (cx, zoom, cy, 0.0, cx , cy, 0.0, 0.0 , 1.0);
    }
 
    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -119,11 +181,9 @@ void display(void)
 
    GLUquadric *quadric;
    quadric = gluNewQuadric();
-
-
    //Sol
    glPushMatrix();
-   glRotatef ((GLfloat) pos3, 0.0, 0.0, 1.0);
+   glRotatef ((GLfloat) rot1, 0.0, 0.0, 1.0);
    glEnable(GL_TEXTURE_2D);
    glBindTexture(GL_TEXTURE_2D, solText);
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -135,9 +195,9 @@ void display(void)
 
    //Planeta
    glPushMatrix();
-   glTranslatef(1.0,0.0,0.0);
-   glTranslatef (3.5*sin(thetar), 2.0*cos(thetar), 0.0f);
-   glRotatef ((GLfloat) pos1, 0.0, 0.0, 1.0);
+   glTranslatef(0.9,0.0,0.0);
+   glTranslatef (3.5*sin(thetar), 3.0*cos(thetar), 0.0f);
+   glRotatef ((GLfloat) rot1, 0.0, 0.0, 1.0);
 
    glEnable(GL_TEXTURE_2D);
    glBindTexture(GL_TEXTURE_2D, venText);
@@ -151,9 +211,9 @@ void display(void)
 
    //Planeta Com lua
    glPushMatrix();
-   glRotatef ((GLfloat) pos2, 0.0, 0.0, 1.0);
-   glTranslatef (5.0, 0.0, 0.0);
-   glRotatef ((GLfloat) pos4, 0.0, 0.0, 1.0);
+   glTranslatef(1.5,0.0,0.0);
+   glTranslatef (5.5*sin(phir), 5.0*cos(phir), 0.0f);
+   glRotatef ((GLfloat) rot2, 0.0, 0.0, 1.0);
 
    glEnable(GL_TEXTURE_2D);
    glBindTexture(GL_TEXTURE_2D, terText);
@@ -163,7 +223,7 @@ void display(void)
    gluSphere(quadric, 0.2 , 20.0, 20.0);
    glDisable(GL_TEXTURE_2D);
 
-   glRotatef ((GLfloat) pos2, 0.0, 0.0, 1.0);
+   glRotatef ((GLfloat) rot3, 0.0, 0.0, 1.0);
    glTranslatef (1.0, 0.0, 0.0);
 
    glEnable(GL_TEXTURE_2D);
@@ -262,7 +322,11 @@ void MyKeyboardFunc(unsigned char Key, int x, int y)
     case '3':
         camera = 3;
         break;
-
+    case '+':
+        zoom -= 0.5;
+        break;
+    case '-':
+        zoom += 0.5;
 
     case ' ':
     break;
@@ -275,19 +339,19 @@ void SpecialKeyboard(int key, int x, int y)
 {
    switch (key) {
       case GLUT_KEY_LEFT:
-         xc = xc-0.5;
+         cx = cx-0.5;
          glutPostRedisplay();
          break;
       case GLUT_KEY_RIGHT:
-         xc = xc+0.5;
+         cx = cx+0.5;
          glutPostRedisplay();
          break;
       case GLUT_KEY_UP:
-         zoom = zoom-0.5;
+         cy = cy-0.5;
          glutPostRedisplay();
          break;
       case GLUT_KEY_DOWN:
-         zoom = zoom+0.5;
+         cy = cy+0.5;
          glutPostRedisplay();
          break;
     }
